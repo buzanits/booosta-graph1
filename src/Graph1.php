77a2f763e26fee407f64d631ffc798a2e19f455b
Peter Buzanits <p.buzanits@epb.at>
2022-07-20 23:09:48 +0200

    code cleanup, add various enhancements

--- src/Graph1.php
+++ src/Graph1.php
@@ -14,10 +14,13 @@ class Graph1 extends \booosta\ui\UI
   protected $height, $width;
   protected $options, $colors, $labels, $tooltip = false;
   protected $mode = 'number';
+  protected $minval, $maxval;
 
-  protected $default_options = 
-    ['grid' => ['hoverable' => 'true', 'borderColor' => '#f3f3f3'], 
-     'series' => ['shadowsize' => '0']];
+  protected $default_options =
+  [
+    'grid' => ['hoverable' => 'true', 'borderColor' => '#f3f3f3'],
+    'series' => ['shadowsize' => '0']
+  ];
 
 
   public function __construct($name, $data = null, $title = null, $height = 300, $width = 400)
@@ -25,7 +28,7 @@ class Graph1 extends \booosta\ui\UI
     parent::__construct();
 
     $this->id = "graph1_$name";
-    if($data === null) $data = [];
+    if ($data === null) $data = [];
     $this->data = $data;
     $this->title = $title;
     $this->height = $height;
@@ -37,10 +40,10 @@ class Graph1 extends \booosta\ui\UI
   {
     parent::after_instanciation();
 
-    if(is_object($this->topobj) && is_a($this->topobj, "\\booosta\\webapp\\Webapp")):
-      $this->topobj->moduleinfo['graph1'] = true;
-      if($this->topobj->moduleinfo['jquery']['use'] == '') $this->topobj->moduleinfo['jquery']['use'] = true;
-    endif;
+    #    if(is_object($this->topobj) && is_a($this->topobj, '\booosta\webapp\Webapp')):
+    #      $this->topobj->moduleinfo['graph1'] = true;
+    #      if($this->topobj->moduleinfo['jquery']['use'] == '') $this->topobj->moduleinfo['jquery']['use'] = true;
+    #    endif;
   }
 
   public function set_title($title) { $this->title = $title; }
@@ -51,107 +54,139 @@ class Graph1 extends \booosta\ui\UI
   public function set_labels($val) { $this->labels = $val; }
   public function set_mode($val) { $this->mode = $val; }
   public function set_tooltip($val = true) { $this->tooltip = $val; }
+  public function set_minval($val = true) { $this->minval = $val; }
+  public function set_maxval($val = true) { $this->maxval = $val; }
 
-  public function show_legend() { $this->set_option('legend', 'show', 'true'); }
+  public function show_legend()
+  {
+    $this->set_option('legend', 'show', 'true');
+  }
 
   public function show_points()
   {
-    $this->set_option('series','points', ['show' => 'true']);
-    $this->set_option('series','lines', ['show' => 'true']);
+    $this->set_option('series', 'points', ['show' => 'true']);
+    $this->set_option('series', 'lines', ['show' => 'true']);
   }
 
-  public function set_option($name, $value, $value1 = null ) 
-  { 
-    if($value1 === null) $this->options[$name] = $value;
+  public function set_option($name, $value, $value1 = null)
+  {
+    if ($value1 === null) $this->options[$name] = $value;
     else $this->options[$name][$value] = $value1;
   }
 
   public function add_data($data)
   {
-    if(is_array($data)) $this->data = array_merge($this->data, $data);
+    if (is_array($data)) $this->data = array_merge($this->data, $data);
     else $this->data[] = $data;
+  }
+
+  public function get_html_includes($libpath = 'lib/modules/graph1')
+  {
+    return;  // disabled
+    return "<script type='text/javascript' src='{$this->base_dir}{$libpath}/js/jquery.flot.js'></script>
+            <script type='text/javascript' src='{$this->base_dir}{$libpath}/js/jquery.flot.time.js'></script>
+            <link rel='stylesheet' type='text/css' href='{$this->base_dir}{$libpath}/jquery.flot.css' media='screen' />";
   }
 
   public function get_htmlonly()
   {
+    #return "<div id='$this->id' style='height: 100%'></div>";
     return "<div id='$this->id' style='height: {$this->height}px;'></div>";
+    #return "<div class='graph1_container' style='width: {$this->width}px; height: {$this->height}px;'> <div id='$this->id' class='graph1_div'></div> </div>";
   }
 
   public function get_js()
   {
-    if(!is_array($this->data)) return '';
+    if (!is_array($this->data)) return '';
     $linearr = [];
 
     // set options depending on $this->mode
-    switch($this->mode):
+    switch ($this->mode):
       case 'date':
         $this->set_option('xaxis', 'mode', 'time');
         $this->set_option('xaxis', 'timeBase', 'seconds');
         $this->set_option('xaxis', 'timeformat', '%d.%m.');
         $this->set_option('xaxis', 'timezone', 'browser');
         $this->convert_x_timestamp();
-      break;
+        break;
       case 'datetime':
         $this->set_option('xaxis', 'mode', 'time');
         $this->set_option('xaxis', 'timeBase', 'seconds');
         $this->set_option('xaxis', 'timeformat', '%d.%m. %H:%M');
         $this->set_option('xaxis', 'timezone', 'browser');
         $this->convert_x_timestamp();
-      break;
+        break;
       case 'time':
         $this->set_option('xaxis', 'mode', 'time');
         $this->set_option('xaxis', 'timeBase', 'seconds');
         $this->set_option('xaxis', 'timeformat', '%H:%M');
         $this->set_option('xaxis', 'timezone', 'browser');
         $this->convert_x_timestamp();
-      break;
+        break;
     endswitch;
 
-    foreach($this->data as $index=>$line):
-      if(!is_array($line)) continue;
+    // if min or max is set, the other must be calculated
+    $calc_minval = false;
+    $calc_maxval = false;
+
+    if(is_numeric($this->minval) && !is_numeric($this->maxval)) $calc_maxval = true;
+    elseif(!is_numeric($this->minval) && is_numeric($this->maxval)) $calc_minval = true;
+
+    foreach ($this->data as $index => $line) :
+      if (!is_array($line)) continue;
       $pointarr = [];
 
-      foreach($line as $point):
-        if(!is_array($point)) continue;
+      foreach ($line as $point) :
+        if (!is_array($point)) continue;
 
         $pointarr[] = "[{$point[0]},{$point[1]}]";
+        if($calc_minval && ($this->minval === null || $this->minval > $point[1])) $this->minval = $point[1];
+        if($calc_maxval && ($this->maxval === null || $this->maxval < $point[1])) $this->maxval = $point[1];
       endforeach;
 
-      if($this->labels[$index]) $label = $this->labels[$index];
-      elseif(is_string($index)) $label = $index;
+      if ($this->labels[$index]) $label = $this->labels[$index];
+      elseif (is_string($index)) $label = $index;
       else $label = '';
 
-      if($label) $linearr[] = "{ label: '$label', data: [" . implode(',', $pointarr) . '], }';
+      if ($label) $linearr[] = "{ label: '$label', data: [" . implode(',', $pointarr) . '], }';
       else $linearr[] = '[' . implode(',', $pointarr) . ']';
     endforeach;
+
+    if(!is_numeric($this->minval) && !is_numeric($this->maxval)):
+      $this->set_option('yaxis', 'autoScale', 'loose');
+    else:
+      $this->set_option('yaxis', 'autoScale', 'none');
+      $this->set_option('yaxis', 'min', $this->minval);
+      $this->set_option('yaxis', 'max', $this->maxval);
+    endif;
 
     $lines = implode(',', $linearr);
 
     // get options
-    $options = $this->get_options(); 
+    $options = $this->get_options();
 
     // show tooltip
-    if($this->tooltip !== false):
-      if($this->tooltip === true) $tooltext = "{label} von {x} = {y}";
+    if ($this->tooltip !== false) :
+      if ($this->tooltip === true) $tooltext = "{label} von {x} = {y}";
       else $tooltext = $this->tooltip;
 
       $tooltext = "'$tooltext'";
       $tooltext = str_replace(['{label}', '{x}', '{y}'], ["' + item.series.label + '", "' + x + '", "' + y + '"], $tooltext);
 
-      $libpath = 'vendor/booosta/graph1/src';
+      $libpath = 'lib/modules/graph1';
       $tpl = file_get_contents("$libpath/tooltip.tpl");
 
       $calculations = '';
-      if(in_array($this->mode, ['date', 'time', 'datetime'])) $calculations = "x = new Date(x*1000).toLocaleDateString('de-DE') ";
+      if (in_array($this->mode, ['date', 'time', 'datetime'])) $calculations = "x = new Date(x*1000).toLocaleString('de-DE', {month: '2-digit', day: '2-digit', hour: '2-digit', minute:'2-digit'}) ";
       $toolcode = str_replace(['{id}', '{tooltext}', '{calculations}'], [$this->id, $tooltext, $calculations], $tpl);
-    else:
+    else :
       $toolcode = '';
     endif;
 
-    if(is_object($this->topobj) && is_a($this->topobj, "\\booosta\\webapp\\webapp")):
-      $this->topobj->add_jquery_ready("\$.plot('#$this->id', [ $lines ], $options); $toolcode");
-    else:
-      return "\$(document).ready(function(){ \$.plot('#$this->id', [ $lines ], $options ); $toolcode});";
+    if (is_object($this->topobj) && is_a($this->topobj, '\booosta\webapp\webapp')) :
+      $this->topobj->add_jquery_ready("\$.plot('#$this->id', [ $lines ],\n $options); $toolcode");
+    else :
+      return "\$(document).ready(function(){ \$.plot('#$this->id', [ $lines ],\n $options ); $toolcode});";
     endif;
   }
 
@@ -171,19 +206,20 @@ class Graph1 extends \booosta\ui\UI
   {
     if(is_array($options)):
       $result = '';
-      foreach($options as $name => $opt) $result .= " $name: " . $this->get_suboptions($opt) . ", ";
-      if(is_array($this->colors)) $result .= 'colors: ["' . implode('", "', $this->colors) . '"], ';
+      foreach ($options as $name => $opt) $result .= " $name: " . $this->get_suboptions($opt) . ", ";
+      if (is_array($this->colors)) $result .= 'colors: ["' . implode('", "', $this->colors) . '"], ';
 
       return " { $result } ";
     endif;
 
     return "'$options'";
+    #return is_numeric($options) ? $options : "'$options'";
   }
 
   protected function convert_x_timestamp()
   {
-    foreach($this->data as $i=>$d)
-      foreach($d as $j=>$data)
+    foreach ($this->data as $i => $d)
+      foreach ($d as $j => $data)
         $this->data[$i][$j][0] = strtotime($data[0]);
   }
 }
